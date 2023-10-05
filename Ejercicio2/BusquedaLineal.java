7615ee5123993ebc0453aed00b00b83ce95a95b4
Haziel Ibares Sanchez <146476173+Hazid635@users.noreply.github.com>
2023-10-05 14:36:40 -0600

    Update BusquedaLineal.java

--- Ejercicio2/BusquedaLineal.java
+++ Ejercicio2/BusquedaLineal.java
@@ -8,21 +8,5 @@ import java.util.List;
  */
 public class BusquedaLineal {
     
-    public static Alumno buscar_Nombre(List<Alumno> alumnos, String nombreABuscar) {
-        for (Alumno alumno : alumnos) {
-            if (alumno.get_Nombre().equals(nombreABuscar)) {
-                return alumno;
-            }
-        }
-        return null; 
-    }
-    
-    public static Asignatura buscar_Clave(List<Asignatura> asignaturas, String claveABuscada) {
-        for (Asignatura asignatura : asignaturas) {
-            if (asignatura.get_Clave().equals(claveABuscada)) {
-                return asignatura;
-            }
-        }
-        return null; 
-    }
+   
 }
