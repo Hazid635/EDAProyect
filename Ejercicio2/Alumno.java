d6af5e742334a2f363bd0fe042306d0f6cc8e33e
Haziel Ibares Sanchez <146476173+Hazid635@users.noreply.github.com>
2023-10-05 13:53:21 -0600

    Update Alumno.java

--- Ejercicio2/Alumno.java
+++ Ejercicio2/Alumno.java
@@ -1,1 +1,34 @@
+package Ejercicio2;
+
+/**
+ * @author Equipo 8
+ */
+public class Alumno {
+
+    private String Nombre;
+    private String numeroCuenta;
+
+    public Alumno(String Nombre, String numeroCuenta) {
+        this.Nombre = Nombre;
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String get_Nombre() {
+        return Nombre;
+    }
+
+    public void set_Nombre(String nombre) {
+        this.Nombre = nombre;
+    }
+
+    public String get_NumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void set_NumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+
+}
 
